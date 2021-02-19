b9a679676ad3e03380e6051adc54e3eb9f2a94ed
Raghuveer Naraharisetti <raghuveernaraharisetti@gmail.com>
2021-02-18 20:30:05 -0600

    add some comments

--- hwk2/src/hwk/Statement.scala
+++ hwk2/src/hwk/Statement.scala
@@ -88,16 +88,22 @@ sealed abstract class Statement extends AbstractSyntaxTree {
     }) + c
   )
 
-  // Raghuveer changes
+  // Homework changes are from here ...
 
   def makeDotLines(s: (Long, Long)): String = s"${s._1} -> ${s._2}"
 
+
+  // class to hold the label properties for a given statement
+  // 1. init
+  // 2. final
+  // 3. flow
+  // 4. reverse flow
   case class LabelProps(label_init: Label, label_final: List[Label], label_flow: List[(Label, Label)]) {
     def reverse_flow = this.label_flow.reverse
   }
 
   var labelProps: LabelProps = _
-  var dotLines: List[String] = List()
+  var dotLines: List[String] = List()  // dot lines notation for the given statement
 
 
   def generateLabelProps {
@@ -111,9 +117,12 @@ sealed abstract class Statement extends AbstractSyntaxTree {
         this.labelProps = LabelProps(this.id, List(this.id), List())
       }
       case IfStmt(_, thenPart, elsePart) => {
+        // first generate for the child i.e then and else parts
         thenPart.generateLabelProps
         elsePart.generateLabelProps
         val label_final = thenPart.labelProps.label_final ++ elsePart.labelProps.label_final
+        // 1. combine then and else parts flow
+        // 2. connect if cond to the then and else part inits
         val label_flow: List[(Label, Label)] = thenPart.labelProps.label_flow ++ elsePart.labelProps.label_flow ++ List((this.id, thenPart.labelProps.label_init), (this.id, elsePart.labelProps.label_init))
         this.labelProps = LabelProps(this.id, label_final, label_flow)
         this.dotLines = this.dotLines ++ label_flow.map(makeDotLines)
@@ -121,10 +130,13 @@ sealed abstract class Statement extends AbstractSyntaxTree {
       case WhileStmt(_, body) => {
         body.generateLabelProps
         // for while init and final are gonna be same
+        // 1. connect while condition to the body init
+        // 2. connect body final points to the while cond
         val label_flow: List[(Label, Label)] = List((this.id, body.labelProps.label_init)) ++ body.labelProps.label_flow ++ body.labelProps.label_final.map((_, this.id))
         this.labelProps = LabelProps(this.id, List(this.id), label_flow)
 
 
+        // generate dot lines inside a subgraph for the dotnotation
         this.dotLines = this.dotLines :+ s"subgraph cluster${this.id} {"
         this.dotLines = this.dotLines ++ List(makeDotLines(this.id, body.labelProps.label_init)) ++ body.labelProps.label_final.map(p => makeDotLines(p, this.id))
         this.dotLines = this.dotLines ++ body.dotLines
@@ -132,14 +144,15 @@ sealed abstract class Statement extends AbstractSyntaxTree {
         this.dotLines = this.dotLines :+ "}"
       }
       case BlockStmt(stmts) => {
+        // generate label properties for each statment in the block
         stmts.foreach(s => s.generateLabelProps)
         val label_init = stmts.head.labelProps.label_init
         // final is same as final statement of the block
         val label_final = stmts.last.labelProps.label_final
-        // generate links with statements, 1->2->3..
+        // connect consecutive statements i.e 1 to 2, 2 to 3, 3 to 4
+        // if these is only statement, set empty set
         var inner_flow: List[(Label, Label)] = if (stmts.length > 1) stmts.sliding(2).toList.map(group => group(0).labelProps.label_final.map(f => (f, group(1).labelProps.label_init))).flatten else List[(Label, Label)]()
         this.dotLines = this.dotLines ++ inner_flow.map(makeDotLines) ++ stmts.map(_.dotLines).flatten
-        // TODO: check the order once everything alright
         val label_flow = inner_flow ++ stmts.map(_.labelProps.label_flow).flatten
         this.labelProps = LabelProps(label_init, label_final, label_flow)
 
@@ -162,9 +175,9 @@ class ControlFlowBuilder() {
   var flow: List[(AbstractSyntaxTree.Label, AbstractSyntaxTree.Label)] = List()
   var prevStatement: Statement = StartStatement()
   var idMap: Map[Long, String] = Map((-1.asInstanceOf[Long] -> "START"))
-  var dotNotationLines: ListBuffer[String] = ListBuffer[String]();
+  var dotNotationLines: ListBuffer[String] = ListBuffer[String]()
 
-  def generateCodeLabels(stmt: Statement): Unit = {
+  def generateCodeLabels(stmt: Statement): Unit = {  // generates display code labels and maps with the assigned id
     stmt match {
       case script: Script => script.stmts.foreach(s => this.generateCodeLabels(s))
       case varDeclStmt: VarDeclStmt => this.idMap = this.idMap + (varDeclStmt.id -> varDeclStmt.toString)
@@ -181,7 +194,7 @@ class ControlFlowBuilder() {
         this.generateCodeLabels(whileStmt.body)
       }
       case blockStmt: BlockStmt => {
-        // just recurse
+        // recurse for all the statements block
         blockStmt.stmts.foreach(s => this.generateCodeLabels(s))
       }
       case emptyStmt: EmptyStmt => this.idMap = this.idMap + (emptyStmt.id -> s"${emptyStmt.id}: Empty Stmt")
@@ -201,7 +214,6 @@ class ControlFlowBuilder() {
 
   def attachToFlow(stmt: Statement, prev_stmt: Statement): Unit = {
     // given a statement and parent, attaches all of the statements flow and parents final to the statement init
-//    this.flow = this.flow ++ prev_stmt.labelProps.label_final.map(p => (p, stmt.labelProps.label_init)) ++ stmt.labelProps.label_flow
     this.dotNotationLines = this.dotNotationLines ++ prev_stmt.labelProps.label_final.map(p => s"${p} -> ${stmt.labelProps.label_init}") ++ stmt.labelProps.label_flow.map(p => s"${p._1} -> ${p._2}")
   }
 
@@ -210,12 +222,15 @@ class ControlFlowBuilder() {
   def build(stmt: Statement, prev_stmt: Statement): Unit = {
     stmt match {
       case script: Script => {
+        // connect the consecutive statements i.e id's
         (List(prev_stmt) ++ script.stmts).sliding(2).toList.map(group => this.build(group(1), group(0)))
       }
       case varDeclStmt: VarDeclStmt => {
+        // connect with given prev statement
         this.dotNotationLines = this.dotNotationLines ++ prev_stmt.labelProps.label_final.map(p => makeDotLine(p, varDeclStmt.labelProps.label_init))
       }
       case exprStmt: ExprStmt => {
+        // connect with given prev statement
         this.dotNotationLines = this.dotNotationLines ++ prev_stmt.labelProps.label_final.map(p => makeDotLine(p, exprStmt.labelProps.label_init))
       }
       case ifStmt: IfStmt => {
